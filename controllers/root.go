7c1367918b8e4a6fa4e5c9f2a2c93fcfc70b4a5f
Ghost <imthaghost@protonmail.com>
2020-08-16 23:07:26 -0700

    [update] cleanup

--- controllers/root.go
+++ controllers/root.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 
 	"github.com/imthaghost/configtree/models"
@@ -11,20 +10,11 @@ import (
 
 // Root ...
 func Root(c echo.Context) error {
-	// var bodyBytes []byte
-	// // request body
-	// if c.Request().Body != nil {
-	// 	// Read the Body content
-	// 	bodyBytes, _ = ioutil.ReadAll(c.Request().Body)
-	// }
-	// // Restore the io.ReadCloser to its original state
-	// c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
 	// visitor to page
 	visitor := models.CreateVisitor(c.RealIP(), c.Request().RemoteAddr)
 	// who is
 	fmt.Println(visitor)
 	// return html
-	return c.Render(http.StatusOK, "index", template.HTML("<p>HTML Test</p>"))
+	return c.String(http.StatusOK, "hi")
 
 }
--- server.go
+++ server.go
@@ -24,34 +24,7 @@ func main() {
 	e.Use(middleware.Logger())
 	// Stream Recovery
 	e.Use(middleware.Recover())
-	// CORS
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-	}))
-	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
-	e.Renderer = t
-	// // static files
-	// e.Static("/", "assets")
-	// // template render
-	// renderer := &TemplateRenderer{
-	// 	templates: template.Must(template.ParseGlob("*.html")),
-	// }
-	// e.Renderer = renderer
-	// // Named route "index"
-	// e.GET("/", func(c echo.Context) error {
-	// 	tracks := api.Track()
-	// 	fmt.Println(tracks)
-	// 	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
 
-	// 		"name":       "Ransom - Lil Tecca & Juice Wrld",
-	// 		"audioURL":   "http://localhost:5000/music/ransom.mp3",
-	// 		"artworkURL": "https://content-images.p-cdn.com/images/public/int/7/8/3/3/00602508243387_1080W_1080H.jpg",
-	// 	})
-
-	// }).Name = "index"
 	e.GET("/", controllers.Root)
 
 	e.Logger.Fatal(e.Start(":5000"))
